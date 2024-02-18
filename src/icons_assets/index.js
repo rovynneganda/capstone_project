04f9cdd27cd49deb207806c3dfbbed8116e432ea
Rovynne Reyes <reyesrobin01234@gmail.com>
2024-02-18 17:28:03 +0800

    main content done

--- src/icons_assets/index.js
+++ src/icons_assets/index.js
@@ -1,7 +1,12 @@
 import logo from "./Logo.svg"
 import headerpic from "./restauranfood.jpg"
-
+import greeksalad from "./greek salad.jpg"
+import bruchetta from "./bruchetta.svg"
+import lemondessert from "./lemondessert.jpg"
 export {
     logo,
     headerpic,
+    greeksalad,
+    bruchetta,
+    lemondessert,
 }
